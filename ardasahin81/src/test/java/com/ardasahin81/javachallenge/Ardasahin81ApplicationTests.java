ca7dfe594ebb5c8c11f9fbe558943ad6f94a3462
arda <ardasahin81@gmail.com>
2022-10-11 18:35:21 +0300

    Initialize project

---
+++ ardasahin81/src/test/java/com/ardasahin81/javachallenge/Ardasahin81ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardasahin81.javachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ardasahin81ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
